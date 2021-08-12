203bf8bc0c468de55ca07c15e5ee12f0caf97eb5
lwch <lwch748@gmail.com>
2021-08-12 17:56:03 +0800

    支持后台运行

--- code/client/main.go
+++ code/client/main.go
@@ -9,16 +9,25 @@ import (
 	"natpass/code/network"
 	"os"
 
+	"github.com/lwch/daemon"
 	"github.com/lwch/runtime"
 )
 
 func main() {
+	bak := flag.Bool("d", false, "backend running")
+	pid := flag.String("pid", "", "pid file dir")
+	user := flag.String("u", "", "daemon user")
 	conf := flag.String("conf", "", "configure file path")
 	flag.Parse()
 
 	if len(*conf) == 0 {
 		fmt.Println("missing -conf param")
 		os.Exit(1)
+	}
+
+	if *bak {
+		daemon.Start(0, *pid, *user, "-conf", *conf)
+		return
 	}
 
 	cfg := global.LoadConf(*conf)
--- code/server/main.go
+++ code/server/main.go
@@ -7,17 +7,26 @@ import (
 	"natpass/code/server/global"
 	"os"
 
+	"github.com/lwch/daemon"
 	"github.com/lwch/logging"
 	"github.com/lwch/runtime"
 )
 
 func main() {
+	bak := flag.Bool("d", false, "backend running")
+	pid := flag.String("pid", "", "pid file dir")
+	user := flag.String("u", "", "daemon user")
 	conf := flag.String("conf", "", "configure file path")
 	flag.Parse()
 
 	if len(*conf) == 0 {
 		fmt.Println("missing -conf param")
 		os.Exit(1)
+	}
+
+	if *bak {
+		daemon.Start(0, *pid, *user, "-conf", *conf)
+		return
 	}
 
 	cfg := global.LoadConf(*conf)
